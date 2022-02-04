f4f29088fe1cbf9c47f0687ffe541e96b3e82219
deepali-m <dmokashi@asu.edu>
2022-02-04 23:41:54 +0530

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -23,7 +23,9 @@ export default class SearchScreen extends Component {
   };
 
   getTransactions = () => {
-    db.collection("transactions").get().then(snapshot => {
+    db.collection("transactions")
+      .get()
+      .then(snapshot => {
         snapshot.docs.map(doc => {
           this.setState({
             allTransactions: [...this.state.allTransactions, doc.data()],
@@ -32,46 +34,19 @@ export default class SearchScreen extends Component {
       });
   };
 
-  handleSearch = async text => {
-    var enteredText = text.toUpperCase().split("");
-    text = text.toUpperCase();
-    this.setState({
-      allTransactions: []
-    });
-    if (!text) {
-      this.getTransactions();
-    }
-    if (enteredText[0] === "B") {
-      db.collection("transactions")
-        .where("book_id", "==", text)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.map(doc => {
-            this.setState({
-              allTransactions: [...this.state.allTransactions, doc.data()],
-            
-            });
-          });
-        });
-    } else if (enteredText[0] === "S") {
-      db.collection("transactions")
-        .where("student_id", "==", text)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.map(doc => {
-            this.setState({
-              allTransactions: [...this.state.allTransactions, doc.data()],
-             
-            });
-          });
-        });
-    }
-  };
+  
 
   
   renderItem = ({ item, i }) => {
-    var date = item.date.toDate().toString().split(" ") .splice(0, 4).join(" ");
-    var transactionType = item.transaction_type === "issue" ? "issued" : "returned";
+    var date = item.date
+      .toDate()
+      .toString()
+      .split(" ")
+      .splice(0, 4)
+      .join(" ");
+
+    var transactionType =
+      item.transaction_type === "issue" ? "issued" : "returned";
     return (
       <View style={{ borderWidth: 1 }}>
         <ListItem key={i} bottomDivider>
@@ -86,13 +61,29 @@ export default class SearchScreen extends Component {
             <View style={styles.lowerLeftContaiiner}>
               <View style={styles.transactionContainer}>
                 <Text
-                  style={[styles.transactionText, {color:item.transaction_type === "issue" ? "#78D304": "#0364F4" }]}
-                >{item.transaction_type.charAt(0).toUpperCase() + item.transaction_type.slice(1)}
+                  style={[
+                    styles.transactionText,
+                    {
+                      color:
+                        item.transaction_type === "issue"
+                          ? "#78D304"
+                          : "#0364F4"
+                    }
+                  ]}
+                >
+                  {item.transaction_type.charAt(0).toUpperCase() +
+                    item.transaction_type.slice(1)}
                 </Text>
                 <Icon
                   type={"ionicon"}
-                  name={ item.transaction_type === "issue" ? "checkmark-circle-outline": "arrow-redo-circle-outline"}
-                  color={item.transaction_type === "issue" ? "#78D304" : "#0364F4"}
+                  name={
+                    item.transaction_type === "issue"
+                      ? "checkmark-circle-outline"
+                      : "arrow-redo-circle-outline"
+                  }
+                  color={
+                    item.transaction_type === "issue" ? "#78D304" : "#0364F4"
+                  }
                 />
               </View>
               <Text style={styles.date}>{date}</Text>
@@ -102,11 +93,9 @@ export default class SearchScreen extends Component {
       </View>
     );
   };
-
-  handleSearch = async text => {
-    // add the code to convert the text to uppercase
-
-
+//complete the function handleSearch()
+  //handleSearch = async text => {
+    
 
     /*if (enteredText[0] === "B") {
       db.collection("transactions")
@@ -116,18 +105,12 @@ export default class SearchScreen extends Component {
           snapshot.docs.map(doc => {
             this.setState({
               allTransactions: [...this.state.allTransactions, doc.data()],
-              lastVisibleTransaction: doc
             });
           });
         });
     } */
-   
-   // complete the else if part
-
-
-  };
-
-
+    
+  
   render() {
     const { searchText, allTransactions } = this.state;
     return (
@@ -142,13 +125,17 @@ export default class SearchScreen extends Component {
             />
             <TouchableOpacity
               style={styles.scanbutton}
-              onPress={() => this.handleSearch(searchText)}
             >
               <Text style={styles.scanbuttonText}>Search</Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.lowerContainer}>
+          <FlatList
+            data={allTransactions}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => index.toString()}
+          />
         </View>
       </View>
     );
